f40b64613cc93058521b976a7f1eb23d47189699
Behnam68engine <behnam68engine@gmail.com>
2024-05-23 04:27:06 +0330

    Add bb10_string_multiplication

---
+++ bb_operations_on_strings_and_numbers_with_exception_handling/bb10_string_multiplication/bb10_string_multiplication.py
@@ -0,0 +1,42 @@
+"""
+Copyright (c) 2024 Behnam
+
+This script is authored by Behnam and is created on May 23, 2024.
+
+
+bb10_string_multiplication
+
+This script demonstrates string multiplication.
+"""
+
+def main():
+    """
+    Main function to demonstrate string multiplication.
+    """
+    a_value = 222  # Assigns integer value 222 to variable a_value.
+    b_value = 23.97653  # Assigns float value 23.97653 to variable b_value.
+    c_value = 143  # Assigns integer value 143 to variable c_value.
+    d_value = 'A'  # Assigns character 'A' to variable d_value.
+    e_value = 'Hello my world'  # Assigns string 'Hello my world' to variable e_value.
+    f_value = 0x8f  # Assigns hexadecimal value 0x8f (143 in decimal) to variable f_value.
+    
+    print("\n\n\n")
+    # print(10*'*')
+    print(100 * '*' ,"\n")  # Prints '*' repeated 100 times.
+    # print(1000*'*')
+    # print(10000*'*')
+    print(10 * 'Behnam ' "\n")  # Prints 'Behnam ' repeated 10 times.
+
+    # Utilizing the unused variables in a print statement
+    print(f"a_value: {a_value}, b_value: {b_value},"
+    f"c_value: {c_value}, d_value: {d_value},"
+    f" e_value: {e_value}, f_value: {f_value}")
+    print("\n\n\n")
+
+
+if __name__ == "__main__":
+    main()
+
+# copyright
+# The copyright command in the code serves as a marker indicating
+# the ownership or copyright of the script.
