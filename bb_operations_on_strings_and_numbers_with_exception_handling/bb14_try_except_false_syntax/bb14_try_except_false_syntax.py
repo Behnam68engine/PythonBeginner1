4566af20d389e5e7651932683c2a7f7d1117ba6c
Behnam68engine <behnam68engine@gmail.com>
2024-07-23 00:28:03 +0330

    Update all files in bb14_try_except_false_syntax with new code

--- bb_operations_on_strings_and_numbers_with_exception_handling/bb14_try_except_false_syntax/bb14_try_except_false_syntax.py
+++ bb_operations_on_strings_and_numbers_with_exception_handling/bb14_try_except_false_syntax/bb14_try_except_false_syntax.py
@@ -4,36 +4,37 @@ This script is authored by Behnam and is created on July 21, 2024.
 # bb14_try_except_false_syntax
 
 This script prompts the user to enter a value, a base number, and a
-power number. It calculates the result of the base number raised to 
-the power of the power number. It handles exceptions if non-integer 
+power number. It calculates the result of the base number raised to
+the power of the power number. It handles exceptions if non-integer
 values are entered. But in the matter of fact it's showing the false
-syntax (for solving it we should write the calculation inside 
-the "try:" part) 
+syntax (for solving it we should write the calculation inside
+the "try:" part)
 """
 
 def main():
     """
     Main function to prompt the user for input and handle exceptions.
     """
-    # Prompting the user to enter a value and storing it in 
+    # Prompting the user to enter a value and storing it in
     # value_input variable
     value_input = input('Enter value:')
+
     # Printing the received value
     print('received =', value_input)
-    
+
     # Trying to convert user input to an integer for the base number
     try:
         base_number = int(input('Enter Number:'))
     # Handling the ValueError exception if non-integer value is entered
-    except:
+    except ValueError:
         print("Error! Enter Integer Value!")
 
     # Trying to convert user input to an integer for the power number
     try:
         power_number = int(input('Enter Pow:'))
-    # Handling any exception (not specified) if non-integer value 
+    # Handling any exception (not specified) if non-integer value
     # is entered
-    except:
+    except ValueError:
         print("Error! Enter Integer Value!")
 
     # Printing the result of the base number raised to the power of
@@ -44,14 +45,13 @@ def main():
 
     # The former code i.e. bb13_try_except.py has a better syntax
     # and don't keep on the wrong code to the error!!
-
-# This is false syntax because print(x**y) is run and runtime error
-# is shown because this line i.e. print(x**Y), so you should write like
-# bb13 (write the "print(base_number ** power_number)" inside try
-# like the bb13_try_except.py not external use like this code!!!'''
+    # This is false syntax because print(x**y) is run and runtime error
+    # is shown because this line i.e. print(x**Y), so you should write like
+    # bb13 (write the "print(base_number ** power_number)" inside try
+    # like the bb13_try_except.py not external use like this code!!!'''
 
 while 1:  # Starts an infinite loop to repeatedly execute the code
     # Running the main function if the script is executed directly
-    if __name__ == "__main__":
-        main()
+    if __name__ == "__main__":  \
         # Calls the main function to execute the script logic
+        main()
